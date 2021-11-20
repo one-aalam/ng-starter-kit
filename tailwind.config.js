f41365839362fe801f429efe0e4a41087525e1ef
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-20 15:42:38 +0530

    feat: Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
