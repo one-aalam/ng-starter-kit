604c906c3660ad36b90242318132ee3293737cc7
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-20 20:51:57 +0530

    feat: Add FortAwesome Icon library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-starter-kit';
+  faCoffee = faCoffee;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CommonsModule } from 'commons'
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonsModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
