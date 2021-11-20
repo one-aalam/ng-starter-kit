bd426e050e04ce962b4c57301ba5663b9730133c
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-20 22:49:16 +0530

    fix: Add ignore for Cypress test cases for Jest

--- jest.lib.config.js
+++ jest.lib.config.js
@@ -3,4 +3,8 @@ const baseConfig = require('./jest.base.config')
 module.exports = {
     ...baseConfig,
     "roots": ["<rootDir>/projects"],
+    "testPathIgnorePatterns": [
+        ...baseConfig.testPathIgnorePatterns,
+        "<rootDir>/projects/*.*/cypress"
+    ],
 }
