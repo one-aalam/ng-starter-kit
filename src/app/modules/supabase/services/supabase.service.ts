80c97d97ba84bf4361585d7fdeff4029e5d3fae9
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-12-05 22:14:59 +0530

    feat: Add Supabase Ng Module

---
+++ src/app/modules/supabase/services/supabase.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AuthChangeEvent, createClient, Session, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '@env/environment'
+import { SupabaseQueryBuilder } from '@supabase/supabase-js/dist/main/lib/SupabaseQueryBuilder';
+import { StorageFileApi } from '@supabase/storage-js/dist/main/lib'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+
+    private client: SupabaseClient;
+
+    constructor() {
+        const { supabaseUrl, supabaseKey } = environment.supabaseClientConfig
+        this.client = createClient(supabaseUrl, supabaseKey);
+    }
+
+    get user() {
+      return this.client.auth.user();
+    }
+
+    get session() {
+      return this.client.auth.session();
+    }
+
+    from(table: 'profiles'): SupabaseQueryBuilder<any> {
+        return this.client.from(table)
+    }
+
+    fromStorage(bucket: string): StorageFileApi {
+        return this.client.storage.from(bucket)
+    }
+
+    getClient(): SupabaseClient {
+        return this.client
+    }
+
+    authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
+      return this.client.auth.onAuthStateChange(callback);
+    }
+}
---
+++ src/app/modules/supabase/supabase.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SupabaseService } from './services/supabase.service'
+
+
+@NgModule({
+  declarations: [],
+  providers: [
+    SupabaseService
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SupabaseModule { }
